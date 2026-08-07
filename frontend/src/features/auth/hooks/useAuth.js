062695ab18d217a01d5e48acf6b0124bb5d70ec0
HashirUmrani <hashir.muhkam@gmail.com>
2026-08-07 14:20:31 +0500

    fixes the intial laoding state error handling

--- frontend/src/features/auth/hooks/useAuth.js
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -44,9 +44,15 @@ export const useAuth = () => {
 
   useEffect(() => {
     const getAndSetUser = async () => {
-      const data = await getMe();
-      setUser(data.user);
-      setLoading(false);
+      try {
+        const data = await getMe();
+        setUser(data.user);
+      } catch {
+        //user is not authorized
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
     };
     getAndSetUser();
   }, []);
