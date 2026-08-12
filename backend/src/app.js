ec270ab537cc66d38654ea1ef0067665f5def0b1
HashirUmrani <hashir.muhkam@gmail.com>
2026-08-12 17:56:20 +0500

    chore: restore backend app file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,12 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const pinoHttp = require("pino-http");
-const cors = require("cors");
 
 const app = express();
 const logger = require("./logger/logger");
 
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = process.env.FRONTEND_URL?.split(",") || [
-  "http://localhost:5173",
-];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) {
-        return callback(null, true);
-      }
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-  }),
-);
 app.use(
   pinoHttp({
     logger,
